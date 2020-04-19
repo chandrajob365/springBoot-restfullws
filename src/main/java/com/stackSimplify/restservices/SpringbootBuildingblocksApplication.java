77044b8f40978b31863ac19423dda2c4702ebd34
Manish Chandra <chandrajob365@gmail.com>
2020-04-19 15:26:07 +0530

    Second Commit

--- src/main/java/com/stackSimplify/restservices/SpringbootBuildingblocksApplication.java
+++ src/main/java/com/stackSimplify/restservices/SpringbootBuildingblocksApplication.java
@@ -2,7 +2,7 @@ package com.stackSimplify.restservices;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+// Test commit
 @SpringBootApplication
 public class SpringbootBuildingblocksApplication {
 
